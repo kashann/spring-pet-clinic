751df0900b4fb510c9c6146f7ab65320f35e2f7f
Kashan <kashi_nadim@yahoo.com>
2020-02-09 22:54:19 +0200

    Added Id property for model objects though BaseEntity class. Closes #9

--- pet-clinic-data/src/main/java/com/kashannadeem/springframework/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/kashannadeem/springframework/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.kashannadeem.springframework.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
