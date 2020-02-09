8a161e5ff831ba6a7dc5b04cacfa4d94750649f5
Kashan <kashi_nadim@yahoo.com>
2020-02-09 21:24:37 +0200

    Added POJOs. Closes #2.

---
+++ src/main/java/com/kashannadeem/springframework/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.kashannadeem.springframework.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/kashannadeem/springframework/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.kashannadeem.springframework.springpetclinic.model;
+
+public class Vet extends Person {
+}
