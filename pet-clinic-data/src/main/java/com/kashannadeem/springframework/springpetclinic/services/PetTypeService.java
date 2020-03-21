89e7206d28c309e73936ba1eaea7d40ab6f2cc89
Kashan <kashi_nadim@yahoo.com>
2020-03-21 18:57:35 +0200

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/com/kashannadeem/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.kashannadeem.springframework.springpetclinic.services;
+
+import com.kashannadeem.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
