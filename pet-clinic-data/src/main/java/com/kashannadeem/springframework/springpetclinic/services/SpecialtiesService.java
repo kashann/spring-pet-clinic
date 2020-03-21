5ced19c9f7c274f7f649ec3c3e2fcdaf11aedfb1
Kashan <kashi_nadim@yahoo.com>
2020-03-21 19:32:28 +0200

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/kashannadeem/springframework/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.kashannadeem.springframework.springpetclinic.services;
+
+import com.kashannadeem.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
