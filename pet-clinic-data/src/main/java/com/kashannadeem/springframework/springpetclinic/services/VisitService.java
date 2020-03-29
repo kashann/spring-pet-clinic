f2f02a26ef27964c397360e3db9a372a4a324c74
Kashan <kashi_nadim@yahoo.com>
2020-03-29 05:25:49 +0300

    Implemented Map Impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/kashannadeem/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.kashannadeem.springframework.springpetclinic.services;
+
+import com.kashannadeem.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
