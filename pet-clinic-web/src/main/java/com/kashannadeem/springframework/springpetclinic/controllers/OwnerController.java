6b8573af2ad4a7da24f97474f82c14b2711d6e1a
Kashan <kashi_nadim@yahoo.com>
2020-04-21 22:27:47 +0300

    Displaying owner records #53. Still needs work

--- pet-clinic-web/src/main/java/com/kashannadeem/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/kashannadeem/springframework/springpetclinic/controllers/OwnerController.java
@@ -1,9 +1,14 @@
 package com.kashannadeem.springframework.springpetclinic.controllers;
 
+import com.kashannadeem.springframework.springpetclinic.model.Owner;
+import com.kashannadeem.springframework.springpetclinic.model.Pet;
 import com.kashannadeem.springframework.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,5 +30,13 @@ public class OwnerController {
     public String findOwners() {
 
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 }
